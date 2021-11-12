727b70480ded676b7006b9c3d1298a6a5559649e
abir <abir.cse.cou@gmail.com>
2021-11-12 15:39:10 +0100

    Delete Order added

--- index.js
+++ index.js
@@ -97,13 +97,13 @@ async function run() {
             res.send(myorders);
         })
 
-        // delete order
-        // app.delete('/myorders/:id', async(req,res) => {
-        //     const id = req.params.id;
-        //     const query = {_id : ObjectId(id)};
-        //     const result = await ordersCollection.deleteOne(query);
-        //     res.json(result);
-        // })
+        delete order
+        app.delete('/myorders/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
 
          
 
