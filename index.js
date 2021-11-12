160e815455fe2dcc898af7841ac3c0f6dc510c17
abir <abir.cse.cou@gmail.com>
2021-11-12 01:50:13 +0100

    addProduct, makeAdmin, purchase, Veiw all myorder implemented

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var cors = require('cors');
 const app = express();
 const { MongoClient } = require('mongodb');
 
+const ObjectId = require('mongodb').ObjectId;
+
 const port = process.env.PORT || 5000;
 
 // use middlewire
@@ -21,22 +23,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        
         const database = client.db('dreamHouseDb');
         const housesCollection = database.collection('houses');
         const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
 
         app.get('/houseshomepage', async(req,res) => {
             const cursor = housesCollection.find({}).limit(6);
-            const houses = await cursor.toArray();
-
-            res.send(houses);
+            try{
+                const houses = await cursor.toArray();
+                res.send(houses);
+            }
+            catch(error) {
+                return res.send(error.message);
+            }
         })
 
         app.get('/allhouses', async(req,res) => {
             const cursor = housesCollection.find({});
-            const houses = await cursor.toArray();
-
+            try {
+                const houses = await cursor.toArray();
             res.send(houses);
+            }
+            catch(error) {
+
+            }
         })
 
 
@@ -46,34 +58,84 @@ async function run() {
             res.send("post hitted");
         })
 
-        // app.get('/purchase', async(req,res) =>{
-        //     const email = req.query.email;
-        //     const query = { email:email};
+        // app.get('/purchase/:id', async(req,res) =>{
+        //     const id = req.params._id;
+        //     const query = { id:id};
+        //     const house = await housesCollection.findOne(query);
+        //     res.json(house);
         // })
 
-        // app.post('/purchase', async(req,res) =>{
-        //     const email = req.query.email;
-        //     const query = { email:email};
+            // GET Single house
+            app.get('/houses/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const house = await housesCollection.findOne(query);
+            res.json(house);
+        })
+
+        // // Get ordered houses
+        // app.get('/orderedhouses/:id' , async(res,res) => {
+
         // })
 
+        // orders collection
+        app.post('/orders', async(req,res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        })
+
+        // users orders
+        app.get('/myorders/:email', async(req,res) => {
+            const email = req.params.email;
+            const query = {clientEmail: email} ;
+
+            const cursor = ordersCollection.find(query);
+            const myorders= await cursor.toArray();
+
+            res.send(myorders);
+        })
+
+        // delete order
+        // app.delete('/myorders/:id', async(req,res) => {
+        //     const id = req.params.id;
+        //     const query = {_id : ObjectId(id)};
+        //     const result = await ordersCollection.deleteOne(query);
+        //     res.json(result);
+        // })
+
+         
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const result = await usersCollection.insertOne(user);
+            try {
+                const result = await usersCollection.insertOne(user);
             console.log(result);
             res.json(result);
+            console.log(' post user working');
+            }
+            catch(error) {
+                res.send(error);
+            }
         });
 
+        
         app.put('/users', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
             const options = { upsert: true };
             const updateDoc = { $set: user };
-            const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.json(result);
+            try {
+                const result = await usersCollection.updateOne(filter, updateDoc, options);
+                res.json(result);
+            }
+            catch(error) {
+                res.send(error);
+            }
         });
 
-
+        // admin role 
         app.put('/users/admin', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
@@ -83,6 +145,7 @@ async function run() {
 
         });
 
+       
 
 
         // check admin
